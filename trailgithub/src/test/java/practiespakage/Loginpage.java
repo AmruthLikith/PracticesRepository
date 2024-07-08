48b377c7acd4e81fe829ef36dbb4d9b1654a56d3
Amruth <amruth1229@gmail.com>
2024-07-08 13:06:23 +0530

    comt

--- trailgithub/src/test/java/practiespakage/Loginpage.java
+++ trailgithub/src/test/java/practiespakage/Loginpage.java
@@ -1,8 +1,11 @@
 package practiespakage;
 
 public class Loginpage {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+
         System.out.println(" Hello welcome Buddy");
-    }
-    //Hello World
+
+            System.out.println("Test Pass : New Sensor Record is Present");
+
+            System.out.println("Test Fail : New Sensor Record is Not Present");
 }
